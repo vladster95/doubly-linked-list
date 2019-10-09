c8b948ebd9565ae84f89fe893389eda7a25d0f8f
vladster95 <V.L.1655@gmail.com>
2019-10-09 19:35:02 +0300

    init my solution

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,94 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.d = new Node([], 0, 1);
+        this.length = this.d.data.length;
+        this._head;
+        this._tail;
+    }
 
-    append(data) {}
+    append(data) {
+        if (this.isEmpty()) {
+            this.d.data.push(data);
+            this.length = this.d.data.length;
+            this._head = this.d;
+            this._tail = this.d;
+        } else {
+            this.d.data.push(data);
+            this.length = this.d.data.length;
 
-    head() {}
+        }
+        return this;
+    }
 
-    tail() {}
+    head() {
+        if (this.d.data.length > 0) {
+            return this.d.data[0];
+        } else {
+            return null;
+        }
+    }
 
-    at(index) {}
+    tail() {
+        if (this.d.data.length > 0) {
+            return this.d.data[this.d.data.length - 1];
+        } else {
+            return null;
+        }
+    }
 
-    insertAt(index, data) {}
+    at(index) {
+        if (this.d.data[index] != null) {
+            return this.d.data[index];
+        } else {
 
-    isEmpty() {}
+        }
+    }
 
-    clear() {}
+    insertAt(index, data) {
+        if (this.d.data[index] != null) {
+            this.d.data.splice(index, 0, data);
+            this.length = this.d.data.length;
+        } else {
 
-    deleteAt(index) {}
+        }
+        return this;
+    }
 
-    reverse() {}
+    isEmpty() {
+        if (this.length) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
-    indexOf(data) {}
+    clear() {
+        this.d.data = [];
+        this.length = 0;
+        return this;
+    }
+
+    deleteAt(index) {
+        this.d.data.splice(index, 1);
+        this.length = this.d.data.length;
+        return this;
+    }
+
+    reverse() {
+        this.d.data.reverse();
+        return this;
+    }
+
+    indexOf(data) {
+        for (let i = 0; i < this.d.data.length; i++) {
+            if (this.d.data[i] === data) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
 
 module.exports = LinkedList;
